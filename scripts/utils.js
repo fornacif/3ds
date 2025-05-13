6dfcdae24472fc3c96d1f8d76614163c7ee9a183
François Fornaciari <francois.fornaciari@gmail.com>
2025-05-13 13:53:14 +0200

    update

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,12 +4,8 @@ export function transformImageSrc(imageSrc) {
     }
 
     imageSrc = imageSrc.replace('width=750', 'width=2000');
-    imageSrc = imageSrc.replace('format=jpeg', 'format=webply');
+    imageSrc = imageSrc.replace('format=jpg', 'format=webply');
     imageSrc = imageSrc.replace('optimize=medium', 'optimize=high');
-
-    if (!imageSrc.includes('?')) {
-        imageSrc += '?format=webply&optimize=high';
-    }
     
     return imageSrc;
 }
