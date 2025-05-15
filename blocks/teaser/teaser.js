cf31b9ef7caa81e9eb40acbbfac50f5cb7966831
François Fornaciari <francois.fornaciari@gmail.com>
2025-05-15 13:44:25 +0200

    update

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -15,7 +15,7 @@ export default function decorate(block) {
         <div class="content-container">
             <h1 data-aue-prop="title" data-aue-type="text">${config.title}</h1>
             <p data-aue-prop="description" data-aue-type="text">${config.description}</p>
-            <a data-aue-prop="buttontext" data-aue-type="text" href="${config.buttonlink}" class="learn-more-btn">${config.buttontext}</a>
+            <a data-aue-prop="buttonText" data-aue-type="text" href="${config.buttonlink}" class="learn-more-btn">${config.buttontext}</a>
         </div>
     </section>
   `);
