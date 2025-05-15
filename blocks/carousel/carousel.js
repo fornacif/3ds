a9d6c487f1f8cfad13eab0ad40a39e4ee145eb4a
François Fornaciari <francois.fornaciari@gmail.com>
2025-05-15 22:45:13 +0200

    update

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -18,9 +18,9 @@ export default function decorate(block) {
       <img class="slide-image" src="${image}?format=webply&optimize=high&smartcrop=1795x500"></img>
       <div class="slide-overlay"></div>
       <div class="slide-content">
-          <h1 data-aue-prop="title" data-aue-type="text">${title}</h1>
-          <p>${description}</p>
-          <button class="cta-button">${buttonText}</button>
+          <h1 data-aue-label="Title" data-aue-prop="title" data-aue-type="text">${title}</h1>
+          <p data-aue-label="Description" data-aue-prop="description" data-aue-type="text">${description}</p>
+          <button data-aue-label="Call to Action" data-aue-prop="buttonText" data-aue-type="text" class="cta-button">${buttonText}</button>
       </div>
     `;
     moveInstrumentation(row, slideElement); 
