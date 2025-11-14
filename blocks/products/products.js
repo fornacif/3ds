1a01228b5c93b922c92291011a5f1fab5c1c3974
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-14 14:32:07 +0100

    update

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
   const content = document.createRange().createContextualFragment(`
     <div class="articles-grid">
-      Articles
+      Products
     </div>
   `);
 
